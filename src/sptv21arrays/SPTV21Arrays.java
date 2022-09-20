8f997eb4aecbd9befab04d7653025642642dd79f
Your Name <you@example.com>
2022-09-20 15:20:40 +0300

    Добавил сумму строк

--- src/sptv21arrays/SPTV21Arrays.java
+++ src/sptv21arrays/SPTV21Arrays.java
@@ -8,7 +8,7 @@ public class SPTV21Arrays {
 
     
     public static void main(String[] args) {
-        System.out.println("-----Demo arrays-----");
+        System.out.println("-----Массив-----");
         int[] arr1 = new int[10];
         Random random = new Random();
         for(int i = 0; i<arr1.length; i++){
@@ -37,39 +37,35 @@ public class SPTV21Arrays {
         System.out.println("Минимальное число в массиве = "+min);
         System.out.println("Максимальное число в массиве = "+max);
         
-        System.out.println("------Demo arrays[][]------");
+        System.out.println("------Двумерный массив[][]------");
         
         int min2 = 10;
         int max2 = 0;
         int sum2 = 0;
-        
-        
+                
         
         int arr2[][] = new int[10][10];
         for(int i = 0;i<arr2.length ;i++){
             for(int j=0;j<arr2[i].length; j++){
-                arr2[i][j]=random.nextInt(1000);
-                
+                arr2[i][j]=random.nextInt(10);   
             }
         }
         for(int i = 0;i<arr2.length;i++){
+            int lSum = 0;
             for(int j=0;j<arr2[i].length; j++){
-                System.out.printf("%4d",arr2[i][j]);{
-                if (min > arr2[i][j]) min = arr2[i][j];
-                if (max < arr2[i][j]) max = arr2[i][j];
-                sum += arr2[i][j];
-            }
-                
-                
-                
-            }
-            System.out.println();
+                System.out.printf("%4d",arr2[i][j]);
+                sum2 += arr2[i][j];
+                lSum += arr2[i][j];
+                if (min2 > arr2[i][j]) min2 = arr2[i][j];
+                if (max2 < arr2[i][j]) max2 = arr2[i][j];
+           }
+            System.out.println(" |Сумма линии = "+lSum);
         }
-        System.out.println("max ="+max);
-        System.out.println("min ="+min);
-        System.out.println("sum ="+sum);
+        System.out.println("max ="+max2);
+        System.out.println("min ="+min2);
+        System.out.println("sum ="+sum2);
         
-        
+          
     }
     
 }
